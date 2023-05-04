2458774f1ac6c00452da5dd9cb7ed5db9fc42bce
Kazi Solayman <kazisolayman123@gmail.com>
2023-05-04 17:58:02 +0600

    data created

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 app.get('/', (req, res) => {
     res.json({name: "solah"})
+})
+const data = require('./data/data.json')
+
+app.get('/chefs', (req, res) => {
+    res.send(data)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = data.find(chef => chef.id == id);
+    res.send(chef);
 })
 app.listen(5000, () => {
     console.log("server is running on port 5000")
